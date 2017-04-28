6dcec9546cb9e0a3274340f31cb0b807ec02911b
rollingflamingo <andrea.storace.uni@gmail.com>
2017-04-28 22:58:33 +0200

    Create list.h

---
+++ list.h
@@ -0,0 +1,34 @@
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+#include <string> 
+
+using namespace std; 
+ 
+namespace list{
+
+/**************************** Tipi e costanti *********************************************/
+typedef string Elem;      // tipo base, compatibile con il tipo Label dell'grafo 
+
+const Elem emptyElem = "$#$#$";
+
+struct list {
+   
+};
+
+typedef list List;
+
+// ATTENZIONE: per la realizzazione del laboratorio 8 non vi serviranno tutte le seguenti funzioni ma solo un sottoinsieme
+void clear(List&);          /* "smantella" la lista */
+void set(int, Elem, List&); /* modifica l'elemento in posizione pos */
+void add(int, Elem, List&); /* inserisce l'elemento in posizione pos, shiftando a destra gli altri elementi */
+void addBack(Elem, List&);  /* inserisce l'elemento alla fine della lista */
+void addFront(Elem, List&); /* inserisce l'elemento all'inizio della lista */
+void removePos(int, List&); /* cancella l'elemento in posizione pos dalla lista  */
+Elem get(int, const List&);	/* restituisce l'elemento in posizione pos */
+List createEmpty();         /* crea la lista vuota e la restituisce */
+bool isEmpty(const List&);
+int size(const List&);
+}
+
+void printList(const list::List&);
